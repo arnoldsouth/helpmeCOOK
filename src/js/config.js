995b22141caaf469ebd703b46715ad46d124f749
arnoldsouth <arnoldsouth@gmail.com>
2022-02-01 17:00:47 -0600

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RESULTS_PER_PAGE = 10;
+export const API_KEY = 'b73f5fa3-0038-4f73-9489-e8bbd5f4d5a1';
+export const MODAL_CLOSE_SEC = 2.5;
